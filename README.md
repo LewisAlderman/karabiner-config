52e3f1e30bed9ba33b1d573cd120acf2365330d3
Lewis Alderman <lewisalderman95@gmail.com>
2024-10-09 23:27:35 +0100

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Sources of inspiration for what can/is done using karabiner.ts:
+
+* [The author of karabiner.ts&#39; own config](https://github.com/evan-liu/karabiner-config/blob/main/karabiner-config.ts)
+* [Another popular config](https://github.com/nikitavoloboev/config/blob/main/karabiner/karabiner.ts)
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import {
+  layer,
+  map,
+  NumberKeyValue,
+  rule,
+  withMapper,
+  writeToProfile,
+} from 'karabiner.ts'
+
+// ! Change '--dry-run' to your Karabiner-Elements Profile name.
+// (--dry-run print the config json into console)
+// + Create a new profile if needed.
+writeToProfile('--dry-run', [
+  // It is not required, but recommended to put symbol alias to layers,
+  // (If you type fast, use simlayer instead, see https://evan-liu.github.io/karabiner.ts/rules/simlayer)
+  // to make it easier to write '←' instead of 'left_arrow'.
+  // Supported alias: https://github.com/evan-liu/karabiner.ts/blob/main/src/utils/key-alias.ts
+
+
+  rule('Key mapping').manipulators([
+    // config key mappings
+    map(1).to(1)
+  ]),
+])
